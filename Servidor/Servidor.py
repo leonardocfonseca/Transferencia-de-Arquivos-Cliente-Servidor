74b3fa85e5a857734b619b75f594cab441c9001a
Leonardo Carvalho Fonseca <162765033+leonardocfonseca@users.noreply.github.com>
2024-06-15 16:40:20 -0300

    Update Servidor.py
    Buffer de recepção e remoção do laço de repetição no match case

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -7,8 +7,7 @@ import os
 # Primeramente ela envia os nomes dos arquivos estão no servidor e espera receber o nome do arquivo escolhido
 # Após receber o nome, ela abre este mesmo arquivo em modo de leitura binaria e vai enviando o conteúdo do arquivo
 def enviarArquivo():
-    diretorio_servidor = os.path.abspath('Servidor')
-    lista_arquivos = os.listdir(diretorio_servidor)
+    lista_arquivos = os.listdir('Servidor')
 
     for i in range(len(lista_arquivos)-1):
         conexão.send(lista_arquivos[i].encode())
@@ -23,15 +22,16 @@ def enviarArquivo():
 
         sleep(0.1)
         #Enviando conteúdo do arquivo
-        caminho_arquivo = os.path.join(diretorio_servidor, nome_arquivo)
+        caminho_arquivo = os.path.join('Servidor', nome_arquivo)
         with open(caminho_arquivo, 'rb') as arquivo:
             buffer = arquivo.read()
             tamanho = str(len(buffer))
             conexão.send(tamanho.encode())
             conexão.send(buffer)
 
-        print('O arquivo "' + nome_arquivo + '" foi enviado com sucesso')
-        
+        print('O arquivo "',nome_arquivo,'" foi enviado com sucesso')
+
+
 
 # Função que recebe o nome do arquivo selecionado, abre ele em modo de escrita binaria
 # e vai escrevendo nele o conteúdo do arquivo recebido pelo cliente
@@ -42,21 +42,21 @@ def receberArquivo():
             break
         
         caminho_arquivo = os.path.join('Servidor', nome_arquivo)
-        print('Caminho arquivo ->',caminho_arquivo)
         with open(caminho_arquivo, 'wb') as arquivo:
             tamanho = int(conexão.recv(1024).decode())
             bytes_recebidos = 0
             conteudo_arquivo = b''
             
             while bytes_recebidos < tamanho:
-                dado = conexão.recv(min(100000, tamanho - bytes_recebidos))
+                dado = conexão.recv(5000000)
                 if not dado: 
                     break
                 conteudo_arquivo += dado
                 bytes_recebidos += len(dado)
 
             arquivo.write(conteudo_arquivo)
-        print('Arquivo "' + nome_arquivo + '" foi recebido com sucesso!')  
+        print('\n --> Arquivo "' + nome_arquivo + '" foi recebido com sucesso! <--\n')  
+
 
 
 
@@ -74,20 +74,14 @@ conexão, cliente = objSocket.accept()
 
 print(cliente[0],'acabou de se conectar')
 
-opção_escolhida = ''
+opção_escolhida = conexão.recv(10).decode()
 
+match opção_escolhida:
+    case '1':
+        receberArquivo()
+    case '2':
+        enviarArquivo()
+    case '0':
+        print('Encerrando conexão...')
 
-while True:
-    opção_escolhida = conexão.recv(10).decode()
-    if not opção_escolhida:
-        break
-
-    match opção_escolhida:
-        case '1':
-            receberArquivo()
-        case '2':
-            enviarArquivo()
-        case '0':
-            break
-print('Encerrando conexão...')
 conexão.close()
