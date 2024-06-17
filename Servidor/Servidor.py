58114ac85eca41ad1410fea1d23e7415177c7725
Leonardo Carvalho Fonseca <162765033+leonardocfonseca@users.noreply.github.com>
2024-06-16 22:19:15 -0300

    Update Servidor.py
    Resumindo mais o código...ou não

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -14,14 +14,11 @@ def enviarArquivo():
         sleep(0.1)
     conexão.send('<END>'.encode())
 
-    # Recebendo o nome do arquivo que o Cliente deseja baixar
     while True:
         nome_arquivo = conexão.recv(1024).decode()
-        if not nome_arquivo:
-            break
-
+        if not nome_arquivo: break
+    
         sleep(0.1)
-        #Enviando conteúdo do arquivo
         caminho_arquivo = os.path.join('Servidor', nome_arquivo)
         with open(caminho_arquivo, 'rb') as arquivo:
             buffer = arquivo.read()
@@ -29,44 +26,34 @@ def enviarArquivo():
             conexão.send(tamanho.encode())
             conexão.send(buffer)
 
-        print('O arquivo "',nome_arquivo,'" foi enviado com sucesso')
-
+        print('O arquivo "' + nome_arquivo + '" foi enviado com sucesso')
 
 
 # Função que recebe o nome do arquivo selecionado, abre ele em modo de escrita binaria
 # e vai escrevendo nele o conteúdo do arquivo recebido pelo cliente
 def receberArquivo():
-    while True:
-        nome_arquivo = conexão.recv(1024).decode()
-        if not nome_arquivo:
-            break
+    nome_arquivo = conexão.recv(1024).decode()
+    caminho_arquivo = os.path.join('Servidor', nome_arquivo)
+
+    with open(caminho_arquivo, 'wb') as arquivo:
+        tamanho = int(conexão.recv(1024).decode())
+        bytes_recebidos = 0
+        conteudo_arquivo = b''
         
-        caminho_arquivo = os.path.join('Servidor', nome_arquivo)
-        with open(caminho_arquivo, 'wb') as arquivo:
-            tamanho = int(conexão.recv(1024).decode())
-            bytes_recebidos = 0
-            conteudo_arquivo = b''
-            
-            while bytes_recebidos < tamanho:
-                dado = conexão.recv(5000000)
-                if not dado: 
-                    break
-                conteudo_arquivo += dado
-                bytes_recebidos += len(dado)
+        while bytes_recebidos < tamanho:
+            dado = conexão.recv(5000000)
+            conteudo_arquivo += dado
+            bytes_recebidos += len(dado)
 
-            arquivo.write(conteudo_arquivo)
-        print('\n --> Arquivo "' + nome_arquivo + '" foi recebido com sucesso! <--\n')  
+        arquivo.write(conteudo_arquivo)
+    print('\n --> Arquivo "' + nome_arquivo + '" foi recebido com sucesso! <--\n')  
 
 
 
 
-endereço = 'localhost'
-porta = 5001
-origem = (endereço, porta)
 
 objSocket = socket(AF_INET, SOCK_STREAM)
-
-objSocket.bind(origem)
+objSocket.bind(('localhost',5001))
 objSocket.listen(1)
 print('O Servidor foi iniciado!')
 
@@ -74,14 +61,13 @@ conexão, cliente = objSocket.accept()
 
 print(cliente[0],'acabou de se conectar')
 
-opção_escolhida = conexão.recv(10).decode()
+opção_escolhida = conexão.recv(10)
 
 match opção_escolhida:
-    case '1':
+    case b'1':
         receberArquivo()
-    case '2':
+    case b'2':
         enviarArquivo()
-    case '0':
+    case b'0':
         print('Encerrando conexão...')
-
-conexão.close()
+        conexão.close()
