77b3bd0e3d36de19305cf4aa6e156c7a7f4b424f
Leonardo Carvalho Fonseca <162765033+leonardocfonseca@users.noreply.github.com>
2024-06-19 23:22:38 -0300

    Update Servidor.py
    Mudanças na função de enviarArquivo

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -3,20 +3,13 @@ from time import sleep
 import os
 
 
-# Função que vai enviar o arquivo para o cliente
-# Primeramente ela envia os nomes dos arquivos estão no servidor e espera receber o nome do arquivo escolhido
-# Após receber o nome, ela abre este mesmo arquivo em modo de leitura binaria e vai enviando o conteúdo do arquivo
+
 def enviarArquivo():
-    lista_arquivos = os.listdir('Servidor')
-
-    for i in range(len(lista_arquivos)-1):
-        conexão.send(lista_arquivos[i].encode())
-        sleep(0.1)
-    conexão.send('<END>'.encode())
-
     while True:
         nome_arquivo = conexão.recv(1024).decode()
-        if not nome_arquivo: break
+        if not nome_arquivo:
+            print('-> Conexão encerrada <-')
+            break
     
         sleep(0.1)
         caminho_arquivo = os.path.join('Servidor', nome_arquivo)
@@ -29,24 +22,28 @@ def enviarArquivo():
         print('O arquivo "' + nome_arquivo + '" foi enviado com sucesso')
 
 
-# Função que recebe o nome do arquivo selecionado, abre ele em modo de escrita binaria
-# e vai escrevendo nele o conteúdo do arquivo recebido pelo cliente
+
+
 def receberArquivo():
-    nome_arquivo = conexão.recv(1024).decode()
-    caminho_arquivo = os.path.join('Servidor', nome_arquivo)
+    while True:
+        nome_arquivo = conexão.recv(1024).decode()
+        if not nome_arquivo:
+            print('--> Conexão encerrada! <--') 
+            break
+        caminho_arquivo = os.path.join('Servidor', nome_arquivo)
 
-    with open(caminho_arquivo, 'wb') as arquivo:
-        tamanho = int(conexão.recv(1024).decode())
-        bytes_recebidos = 0
-        conteudo_arquivo = b''
-        
-        while bytes_recebidos < tamanho:
-            dado = conexão.recv(5000000)
-            conteudo_arquivo += dado
-            bytes_recebidos += len(dado)
+        with open(caminho_arquivo, 'wb') as arquivo:
+            tamanho = int(conexão.recv(1024).decode())
+            bytes_recebidos = 0
+            conteudo_arquivo = b''
+            
+            while bytes_recebidos < tamanho:
+                dado = conexão.recv(5000000)
+                conteudo_arquivo += dado
+                bytes_recebidos += len(dado)
 
-        arquivo.write(conteudo_arquivo)
-    print('\n --> Arquivo "' + nome_arquivo + '" foi recebido com sucesso! <--\n')  
+            arquivo.write(conteudo_arquivo)
+        print('\n --> Arquivo "' + nome_arquivo + '" foi recebido com sucesso! <--\n')  
 
 
 
@@ -70,4 +67,5 @@ match opção_escolhida:
         enviarArquivo()
     case b'0':
         print('Encerrando conexão...')
-        conexão.close()
+
+conexão.close()
