66c54c2b4c39ed498a83b4bdc7ac2f9a09c8dab1
Leonardo Carvalho Fonseca <162765033+leonardocfonseca@users.noreply.github.com>
2024-06-19 23:23:50 -0300

    Update Cliente.py
    Mudanças na função de receberArquivo, entre outras coisas

--- Cliente/Cliente.py
+++ Cliente/Cliente.py
@@ -29,90 +29,55 @@ def selecionarArquivo():
 
 
 
-# Função que vai enviar o arquivo para o servidor
-# Primeiramente ela envia o nome do arquivo para o servidor
-# E depois ela vai enviando o conteúdo do arquivo
+
+
+
 def enviarArquivo():
-    dirArquivo = selecionarArquivo()
-    nome_arquivo = os.path.basename(dirArquivo)
-    sockobj.send(nome_arquivo.encode())
-    sleep(0.5)
-
-    with open(dirArquivo, 'rb') as arquivo:
-        buffer = arquivo.read()
-        tamanho = str(len(buffer))
-        sockobj.send(tamanho.encode())
-        sockobj.send(buffer)
-
-    print('\n--> O Arquivo "' + nome_arquivo + '" foi enviado com sucesso <--\n')
-
-
-
-# Função que recebe como parametro o vetor contendo os nomes dos arquivos que estão no servidor
-# E imprime o nome do arquivo em cada linha
-def exibirArquivos(lista_arquivo):
-    print('\n<-- Arquivos presente no servidor -->\n')
-    for i in lista_arquivo:
-        print(i)
-    print('\n')
-
-
-
-# Função que vai cuidar do input do Cliente caso ele digite o nome do arquivo errado
-# Ela recebe como parâmetro um vetor contendo os nomes dos arquivos
-def inserirNomeArquivo(lista):
     while True:
-        nome_arquivo = input('Informe o arquivo que deseja baixar! [Aperte 0 para encerrar conexão]\n')
-        if nome_arquivo == '0':
-            print('--> Operação Finalizada <--')
-            return '0'
-    
-        if nome_arquivo in lista:
-            return nome_arquivo
+        dirArquivo = selecionarArquivo()
+        nome_arquivo = os.path.basename(dirArquivo)
+        sockobj.send(nome_arquivo.encode())
+        sleep(0.5)
+
+        with open(dirArquivo, 'rb') as arquivo:
+            buffer = arquivo.read()
+            tamanho = str(len(buffer))
+            sockobj.send(tamanho.encode())
+            sockobj.send(buffer)
+
+        print('\n--> O Arquivo "' + nome_arquivo + '" foi enviado com sucesso <--\n')
         
-        print('\n--> O arquivo "'+ nome_arquivo +'" não consta na base de dados do servidor! <--\n')    
-
-
-
-def listarArquivos():
-    lista_arquivos = []
-    while True:
-        nome = sockobj.recv(1024).decode()
-        if not nome or nome == '<END>':
+        resp = input('Deseja enviar mais um arquivo? [S/N]: ').upper()
+        if resp == 'N':
+            print('-> Conexão encerrada <-')
             break
-        lista_arquivos.append(nome)
-    return lista_arquivos
+
+
+
 
 
 
 def receberArquivo():
-    lista_de_arquivos = listarArquivos()
-    
-    if lista_de_arquivos:
-        exibirArquivos(lista_de_arquivos)
-
-        while True:
-            nome_arquivo = inserirNomeArquivo(lista_de_arquivos)
-            if nome_arquivo == '0':
-                break
-            sockobj.send(nome_arquivo.encode())
+    while True:
+        nome_arquivo = input('Informe o arquivo que deseja baixar! [Aperte 0 para encerrar conexão]\n')
+        if nome_arquivo == '0':
+            print('--> Operação Finalizada <--')
+            break
+        sockobj.send(nome_arquivo.encode())
+        
+        caminho_arquivo = os.path.join('Cliente', nome_arquivo)
+        with open(caminho_arquivo, 'wb') as arquivo:
+            tamanho = int(sockobj.recv(1024).decode())
+            bytes_recebidos = 0
+            conteudo_arquivo = b''
             
-            caminho_arquivo = os.path.join('Cliente', nome_arquivo)
-            with open(caminho_arquivo, 'wb') as arquivo:
-                tamanho = int(sockobj.recv(1024).decode())
-                bytes_recebidos = 0
-                conteudo_arquivo = b''
+            while bytes_recebidos < tamanho:
+                dado = sockobj.recv(5000000)
+                conteudo_arquivo += dado
+                bytes_recebidos += len(dado)
                 
-                while bytes_recebidos < tamanho:
-                    dado = sockobj.recv(5000000)
-                    conteudo_arquivo += dado
-                    bytes_recebidos += len(dado)
-                    
-                arquivo.write(conteudo_arquivo)
-            print('\n --> Arquivo "' + nome_arquivo + '" foi baixado com sucesso! <--\n')
-            
-    else:
-        print('\n--> Não há arquivos presentes no servidor! <--\nOperação Cancelada!\n')
+            arquivo.write(conteudo_arquivo)
+        print('\n --> Arquivo "' + nome_arquivo + '" foi baixado com sucesso! <--\n')
 
 
 
@@ -131,7 +96,6 @@ match opção:
         receberArquivo()
     case '0':
         sockobj.send(opção.encode())
-        # Encerrar programa
+        print('Encerrando conexão...')
 
-print('Encerrando conexão...')
 sockobj.close()            
